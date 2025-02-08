744ccdf48faeb25de8c979caf0bcfb3d3bd75495
merwil vegas <com2pa@gmail.com>
2025-02-08 11:20:16 -0400

    configuracion de produccion y desarrollo

---
+++ config.js
@@ -0,0 +1,13 @@
+const PAGE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://micro-users.onrender.com'
+    : 'http://localhost:5173';
+
+    const MONGO_URL = process.env.NODE_ENV === 'production'
+    ? process.env.MONGO_URI_PRODUC
+    : process.env.MONGO_URI_TEST
+
+module.exports = {
+    PAGE_URL,
+    MONGO_URL,
+}
