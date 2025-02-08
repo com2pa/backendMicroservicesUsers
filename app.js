3e2f57108e803075af18c563e748e1f889f6921e
merwil vegas <com2pa@gmail.com>
2025-02-08 11:19:24 -0400

    endpoint, conexion con bd

---
+++ app.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const usersRouter = require('./controllers/users');
+const cors = require('cors')
+const cookieParser = require('cookie-parser');
+const { usertExtractor } = require('./middleware/auth');
+const { MONGO_URL } = require('./config');
+
+const loginRouter = require('./controllers/login');
+const refresRouter = require('./controllers/refres');
+const logoutRouter = require('./controllers/logout');
+
+
+
+
+// const morgan=require('morgan')
+// conexion base de datos
+(async () => {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log('Conectado a MongoDB :)');
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+app.use(cors())
+app.use(express.json());
+app.use(cookieParser())
+// app.use(morgan('tiny'))
+
+// rutas backEnd
+app.use('/api/users', usersRouter);
+app.use('/api/logout', logoutRouter);
+app.use('/api/login', loginRouter);
+app.use('/api/refres', usertExtractor, refresRouter)
+
+
+
+
+
+module.exports = app;
