b9769b7d14af707bf523497dd705c9585a24a5b1
merwil vegas <com2pa@gmail.com>
2025-02-22 16:39:06 -0400

    modificado

--- controllers/logout.js
+++ controllers/logout.js
@@ -1,10 +1,10 @@
 const logoutRouter = require('express').Router();
 
 logoutRouter.get('/',async(request,response)=>{
-    const cookies = request.cookies
+    const cookies = request.cookies.accesstoken
     console.log(cookies,'logout')
     // verifico si la cookies existe
-    if(!cookies.accesstoken){
+    if(!cookies){
         // si no existe la propiedad accesstoken
         return response.status(401).json('usted no a iniciado sesion! ')
     }
